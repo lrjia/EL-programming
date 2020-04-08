fe38440d0d27affa667588f29054c86c0bc2e486
dracoooooo <191250037@smail.nju.edu.cn>
2020-04-08 15:46:09 +0800

    完善了一下评论按钮 滚动显示在搞了

---
+++ scripts/mode3.js
@@ -0,0 +1,30 @@
+function is_valid(){
+
+    //输入姓名为空
+    var user_name = document.forms["user_comment"]["user-name"].value;
+    if (user_name === ""){
+        alert("Please enter your name.");
+        return false;
+    }
+    //输入评论为空
+    var user_comment = document.forms["user_comment"]["comment-text"].value;
+    if (user_comment === ""){
+        alert("Please enter your comment.");
+        return false;
+    }
+    //成功弹出提示框并退出窗口
+    var html_success = "<div class=\"alert alert-success alert-dismissible fade show\">\n" +
+        "  <button type=\"button\" class=\"close\" data-dismiss=\"alert\" id='close_button_in_alert'>&times;</button>\n" +
+        "  <strong>Success</strong>\n" +
+        "</div>";
+    var close_button = document.getElementById("close_button");
+    if(user_name !== "" && user_comment !== ""){
+        close_button.click();
+        $("body").append(html_success);
+        var close_button_in_alert = document.getElementById("close_button_in_alert");
+        setInterval(function () {
+            close_button_in_alert.click();
+        },3000);
+        return true;
+    }
+}

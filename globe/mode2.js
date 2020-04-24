1c8c298d419bdaed90ac62e21f18e0f0f3f6e963
Chenzihong <191250014@smail.nju.edu.cn>
2020-04-24 17:12:12 +0800

    按钮

--- globe/mode2.js
+++ globe/mode2.js
@@ -44,19 +44,37 @@ let returnButton = document.getElementById("returnButton");
 let nextButton = document.getElementById("nextButton");
 let preButton = document.getElementById("preButton");
 let introduction = document.getElementById("introduction");
+let nodeImage=document.getElementById("nodeImage");
 
 
-let timenow = 0;
+let nodeNow = 0;
 // 按钮函数
-function buttonFunc(next,totalDay) {
+function buttonFunc(next, totalDay) {
     return function () {
-        timenow += next;
-        timenow %= totalDay;
-        if (timenow < 0) {
-            timenow += totalDay;
+        // 最初的数据日期，如果修改需要更改
+        let startDate = new Date("2020-01-22");
+        // 如果有新的节点需要在这里添加时间，同时图片的命名也按照下面的时间格式
+        let nodes = ["2019-12-01", "2020-01-05", "2020-01-10", "2020-01-15",
+            "2020-01-23", "2020-02-06", "2020-02-07", "2020-02-16", "2020-03-14",
+            "2020-03-31", "2020-04-04", "2020-04-08", "2020-04-23"];
+        nodeNow += next;
+        nodeNow %= nodes.length;
+        if (nodeNow < 0) {
+            nodeNow += nodes.length;
         }
+        let nodeDate=new Date(nodes[nodeNow]);
+        let timenow = 0;
+        
+        if (nodeDate.getTime()>startDate.getTime()){
+            timenow=Math.round((nodeDate.getTime()-startDate.getTime())/86400000);
+            timenow=Math.min(timenow,totalDay-1);
+        }
+        // 节点的文字介绍
+        introduction.textContent = nodeDate.toDateString();
+        let imageSrc="styles/images/mode2/"+nodes[nodeNow]+".png";
+        nodeImage.setAttribute('src', imageSrc);
+        // debugger;
         settime(globe, timenow)();
-        introduction.textContent = "这里可以显示节点介绍, 节点" + timenow;
     }
 }
 

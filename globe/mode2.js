79df41d4e212f25dc3b0e396483d8c9e06016f71
Chenzihong <191250014@smail.nju.edu.cn>
2020-05-11 22:52:54 +0800

    截图按钮

--- globe/mode2.js
+++ globe/mode2.js
@@ -55,8 +55,8 @@ function buttonFunc(next, totalDay) {
         let startDate = new Date("2020-01-22");
         // 如果有新的节点需要在这里添加时间，同时图片的命名也按照下面的时间格式
         let nodes = ["2019-12-01", "2020-01-05", "2020-01-10", "2020-01-15",
-            "2020-01-23", "2020-02-06", "2020-02-07", "2020-02-16", "2020-03-14",
-            "2020-03-31", "2020-04-04", "2020-04-08", "2020-04-23"];
+            "2020-01-23", "2020-02-06", "2020-02-07", "2020-02-16", "2020-03-14","2020-03-27",
+            "2020-03-31", "2020-04-04", "2020-04-08","2020-04-15", "2020-04-23"];
         nodeNow += next;
         nodeNow %= nodes.length;
         if (nodeNow < 0) {
@@ -77,8 +77,17 @@ function buttonFunc(next, totalDay) {
         settime(globe, timenow)();
     }
 }
-
-returnButton.addEventListener("click", returnToMenu);
-function returnToMenu() {
-    window.location.replace('index.html')
-}
+var countx = 2;
+    function countDown() {
+        //写一个方法，显示倒数秒数  数到0后跳转页面  
+        //每执行一次，count减1
+        countx -= 1;
+        //count=0时，跳转页面
+        if (countx == 0) {
+            window.location.replace('index.html');
+            //window.location.href="index.html";
+            countx = 2;
+        }
+        //设定倒数秒数 
+        setTimeout("countDown()", 1000);
+    }

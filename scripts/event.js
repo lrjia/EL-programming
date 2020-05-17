8b35fdff0ae87c64e82da4bd16a6c1df53878d9b
Chenzihong <191250014@smail.nju.edu.cn>
2020-05-17 18:07:27 +0800

    声音

--- scripts/event.js
+++ scripts/event.js
@@ -1,4 +1,4 @@
-    function fadeIn(element,speed){
+function fadeIn(element,speed){
     if(element.style.opacity !=1){
         var speed = speed || 30 ;
         var num = 0;
@@ -9,15 +9,34 @@
         },speed);
     }
 }
+function fadeOut(element,speed){
+    if(element.style.opacity !=0){
+        var speed = speed || 30 ;
+        var num = 10;
+        var st = setInterval(function(){
+        num--;
+        element.style.opacity = num/10;
+        if(num<=0)  {  clearInterval(st);  }
+        },speed);
+    }
+}
 
 function btnIn(){
     fadeIn(fade,100);
+}
+function btnOut(){
+    fadeOut(fade,100);
 }
 
 function btnIn1(){
     fadeIn(fade1,100);
 }
-
+function btnOut1(){
+    fadeOut(fade1,100);
+}
 function btnIn2(){
     fadeIn(fade2,100);
+}
+function btnOut2(){
+    fadeOut(fade2,100);
 }

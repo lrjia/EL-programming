57416d387c1cd81d2f5f398af9bb2dc9ce72f4fa
lrjia <lrjia2017@gmail.com>
2020-05-26 22:10:37 +0800

    使用压缩的数据

--- globe/InitGlobe.js
+++ globe/InitGlobe.js
@@ -1,33 +1,70 @@
 class InitGlobe {
     constructor(container, init) {
         let globe = new DAT.Globe(container);
-        let data = null; // 储存数据的全局变量
         let initGlobe = this;
+        let loadCount=0;
+        let mapData=null;
+        let dayData=null;
+        
 
         // 加载数据
-        let xhr = new XMLHttpRequest();
-        xhr.open('GET', 'globe/data.json', true);
-        xhr.onreadystatechange = function (e) {
-            if (xhr.readyState === 4 && xhr.status === 200) {
-                data = JSON.parse(xhr.responseText);
-                window.data = data;
-                initGlobe.totalDay = data.length;
-                globe.addData(data[0][1], { format: 'magnitude' });
-                globe.createPoints();
-                globe.animate();
-
-                init();//数据加载完成后执行函数
+        let mapXHR = new XMLHttpRequest();
+        mapXHR.open('GET', 'globe/mapData.json', true);
+        mapXHR.onreadystatechange = function (e) {
+            if (mapXHR.readyState === 4 && mapXHR.status === 200) {
+                mapData = JSON.parse(mapXHR.responseText);
+                loadCount+=1;
+                if (loadCount===2){
+                    loadReady();
+                }
             }
         };
+        mapXHR.send(null);
 
-        xhr.send(null);
-        let settime = function (t) {
-
-            globe.resetData();
-            globe.addData(data[t][1], { format: 'magnitude' });
-            globe.createPoints();
-
+        let dayXHR = new XMLHttpRequest();
+        dayXHR.open('GET', 'globe/dayData.json', true);
+        dayXHR.onreadystatechange = function (e) {
+            if (dayXHR.readyState === 4 && dayXHR.status === 200) {
+                dayData = JSON.parse(dayXHR.responseText);
+                loadCount+=1;
+            }
+            if (loadCount===2){
+                loadReady();
+            }
         };
+        dayXHR.send(null);
+
+        function loadReady(){
+            initGlobe.totalDay = dayData.length;
+            settime(0);
+            globe.createPoints();
+            globe.animate();
+            init();//数据加载完成后执行函数
+        }
+
+        let settime = function (t) {
+            let oneData=getOneDayDate(t);
+            globe.resetData();
+            globe.addData(oneData, { format: 'magnitude' });
+            globe.createPoints();
+        };
+
+        function getOneDayDate(t){
+            let out=new Array();
+            let oneDayNum=dayData[t][1];
+            let count=0;
+            for(let i=0;i<mapData.length;i++){
+                let magnitude=oneDayNum[i];
+                for(let j=0;j<(mapData[i].length)/2;j++){
+                    let latitude=mapData[i][j*2];
+                    let longitude=mapData[i][j*2+1];
+                    out[count++]=latitude;
+                    out[count++]=longitude;
+                    out[count++]=magnitude;
+                }
+            }
+            return out;
+        }
 
         this.globe = globe;
         this.settime = settime;

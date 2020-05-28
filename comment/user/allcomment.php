a4c4fa2d74fe6b39c08118c8554cef595c9cb00b
dracoooooo <191250037@smail.nju.edu.cn>
2020-05-28 17:20:59 +0800

    修复评论区bug

--- comment/user/allcomment.php
+++ comment/user/allcomment.php
@@ -37,19 +37,19 @@ echo '
 	$comment = $db->row($sql);
  }
 
-    $username2  = $_POST['username'];
-    $comment2  = $_POST['comment'];
-    $time2 =  date("Y-m-d H:i:s");
-    echo $username2;
-    echo '
-    <div class="card bg-light mb-3" style="width: 100%;position:absolute;max-height:10.5rem;z-index: 900;">
-        <div class="card-header">
-            <div style="width: 50%;float: left;display: inline;text-overflow: ellipsis;overflow: hidden;white-space: nowrap;" data-toggle="tooltip" data-placement="top" title="$name">'.$name2.'</div>
-            <div style="width: 10%;float: right;display: inline;text-overflow: ellipsis;overflow: hidden;white-space: nowrap;font-size: small">美国</div>
-            <div style="width: 25%;float: right;display: inline;text-overflow: ellipsis;overflow: hidden;white-space: nowrap;font-size: small">'.$time2.'</div>
-        </div>
-        <div class="card-body" style="text-overflow: ellipsis;overflow: hidden;white-space: normal">'.$comment2.'
-        </div>
-    </div>
-';
+//    $username2  = $_POST['username'];
+//    $comment2  = $_POST['comment'];
+//    $time2 =  date("Y-m-d H:i:s");
+//    echo $username2;
+//    echo '
+//    <div class="card bg-light mb-3" style="width: 100%;position:absolute;max-height:10.5rem;z-index: 900;">
+//        <div class="card-header">
+//            <div style="width: 50%;float: left;display: inline;text-overflow: ellipsis;overflow: hidden;white-space: nowrap;" data-toggle="tooltip" data-placement="top" title="$name">'.$name2.'</div>
+//            <div style="width: 10%;float: right;display: inline;text-overflow: ellipsis;overflow: hidden;white-space: nowrap;font-size: small">美国</div>
+//            <div style="width: 25%;float: right;display: inline;text-overflow: ellipsis;overflow: hidden;white-space: nowrap;font-size: small">'.$time2.'</div>
+//        </div>
+//        <div class="card-body" style="text-overflow: ellipsis;overflow: hidden;white-space: normal">'.$comment2.'
+//        </div>
+//    </div>
+//';
 
